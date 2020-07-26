8d51c697c946a1497ab224fc42af849e144eb406
ajkerber27 <ajkerber27@gmail.com>
2020-07-25 21:26:03 -0500

    PyBank Challenge

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,115 +1,61 @@
-# Byte-compiled / optimized / DLL files
-__pycache__/
-*.py[cod]
-*$py.class
+#import dependencies
+import csv
+import os
+#import csv file
+Budget_Data_File = os.path.join("Resources","02-Homework_03-Python_Instructions_PyBank_Resources_budget_data.csv")
 
-# C extensions
-*.so
+#define variables
+total_months = 0
+net_change_list=[]
+greatest_increase=["",0]
+greatest_decrease=["",99999999]
+total_net=0 
+with open (Budget_Data_File) as Budget_Data:
+    reader= csv.reader(Budget_Data)
+    header= next(reader)
+    print(header)
 
-# Distribution / packaging
-.Python
-build/
-develop-eggs/
-dist/
-downloads/
-eggs/
-.eggs/
-lib/
-lib64/
-parts/
-sdist/
-var/
-wheels/
-share/python-wheels/
-*.egg-info/
-.installed.cfg
-*.egg
-MANIFEST
+    #extract header
+    First_Row=next(reader)
+    total_months=1
+    total_net=int(First_Row[1])
+    previous_net=int(First_Row[1])
 
-# PyInstaller
-#  Usually these files are written by a python script from a template
-#  before PyInstaller builds the exe, so as to inject date/other infos into it.
-*.manifest
-*.spec
+    #forloop
+    for row in reader:
+        #tracking the total
+        total_months=total_months+1
+        total_net=total_net+int(row[1])
+        #tracking the net change
+        net_change=int(row[1])-previous_net
+        previous_net=int(row[1])
+        net_change_list=net_change_list+[net_change]
 
-# Installer logs
-pip-log.txt
-pip-delete-this-directory.txt
+        #track increases and decreases
+        if net_change > greatest_increase[1]:
+            greatest_increase[0]=row[0]
+            greatest_increase[1]=net_change
+        if net_change < greatest_decrease[1]:
+            greatest_decrease[0]=row[0]
+            greatest_decrease[1]=net_change
 
-# Unit test / coverage reports
-htmlcov/
-.tox/
-.nox/
-.coverage
-.coverage.*
-.cache
-nosetests.xml
-coverage.xml
-*.cover
-.hypothesis/
-.pytest_cache/
+#calculate average net change
+average_net_change=sum(net_change_list)/len(net_change_list)
 
-# Translations
-*.mo
-*.pot
+#output of data
+output=(
+    f"Financial Analysis\n"
+f"----------------------------\n"
+f"Total Months: {total_months}\n"
+f"Total: ${total_net}\n"
+f"Average  Change: ${average_net_change}\n"
+f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})\n"
+f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})\n"
+)
 
-# Django stuff:
-*.log
-local_settings.py
-db.sqlite3
+print(output)
 
-# Flask stuff:
-instance/
-.webassets-cache
+with open ("Output.txt","w") as textfile: 
+    textfile.write(output)
 
-# Scrapy stuff:
-.scrapy
 
-# Sphinx documentation
-docs/_build/
-
-# PyBuilder
-target/
-
-# Jupyter Notebook
-.ipynb_checkpoints
-
-# IPython
-profile_default/
-ipython_config.py
-
-# pyenv
-.python-version
-
-# celery beat schedule file
-celerybeat-schedule
-
-# SageMath parsed files
-*.sage.py
-
-# Environments
-.env
-.venv
-env/
-venv/
-ENV/
-env.bak/
-venv.bak/
-
-# Spyder project settings
-.spyderproject
-.spyproject
-
-# Rope project settings
-.ropeproject
-
-# mkdocs documentation
-/site
-
-# mypy
-.mypy_cache/
-.dmypy.json
-dmypy.json
-
-# Pyre type checker
-.pyre/
